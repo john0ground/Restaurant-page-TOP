5af719d3caf9b77accd9512e42be3dfa21c70644
John <mjohncorbet@gmail.com>
2023-04-10 20:53:03 +0800

    setup contact page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { displayHome } from './home';
 import { displayMenu } from './menu';
 import './style/home.css';
 import './style/menu.css';
+import './style/contact.css';
 
-displayMenu();
+// displayMenu();
 // displayHome();
