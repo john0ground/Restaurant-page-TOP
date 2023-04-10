0dea94d215db6df4ece031b1c6c8b44846a14273
John <mjohncorbet@gmail.com>
2023-04-10 22:22:06 +0800

    create system for changing webpage

--- src/index.js
+++ src/index.js
@@ -5,4 +5,38 @@ import './style/home.css';
 import './style/menu.css';
 import './style/contact.css';
 
-displayContact();
+displayHome();
+
+const changePages = (() => {
+    const navbar = document.querySelector('nav');
+    
+    const navBreak = document.querySelectorAll('.nav-break');
+    navBreak.forEach((nav) => {
+        nav.style.pointerEvents = 'none';
+    });
+
+    let currentPage = 'Home';
+
+    function displayPage(e) {
+        if (e.target.textContent === currentPage) return;
+        else {
+            currentPage = e.target.textContent;
+
+            const content = document.querySelector('#content');
+            content.innerHTML = '';
+
+            switch (currentPage) {
+                case 'Home':
+                    return displayHome();
+                case 'Menu':
+                    return displayMenu();
+                case 'Contact':
+                    return displayContact();
+            }
+        };
+    }
+    
+    navbar.childNodes.forEach((nav) => {
+        nav.addEventListener('click', displayPage);
+    });
+})();
